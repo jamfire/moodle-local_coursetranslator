a60b5adbb62691a965657fbf836e114f6a090fa1
Kaleb Heitzman <kaleb@jamfire.io>
2022-04-30 11:27:45 -0400

    bumped version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_coursetranslator';  // Full name of the plugin (used for diagnostics).
-$plugin->version      = 2022042600;                // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version      = 2022043000;                // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires     = 2020061500;                // Requires Moodle 3.9 LTS.
 $plugin->supported    = [311, 400];                // Supported Moodle Versions.
 $plugin->maturity     = MATURITY_ALPHA;            // Maturity level.

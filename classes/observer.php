c0241dd47e5cd006b1acbd104609ac5a18c41d0f
Kaleb Heitzman <kaleb@jamfire.io>
2022-04-30 17:57:30 -0400

    added event observer for updating t_lastmodified

---
+++ classes/observer.php
@@ -0,0 +1,132 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Course Translator Observers
+ *
+ * Watch for course, course section, and mod updates
+ *
+ * @package    local_coursetranslator
+ * @copyright  2022 Kaleb Heitzman <kaleb@jamfire.io>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @see        https://docs.moodle.org/dev/Events_API
+ */
+
+class local_coursetranslator_observer {
+
+    /**
+     * Observer for course_updated event
+     *
+     * @param \core\event\course_updated $event
+     * @return void
+     */
+    public static function course_updated(\core\event\course_updated $event) {
+        global $DB;
+
+        // Get params.
+        $objectid = $event->objectid;
+        $objecttable = $event->objecttable;
+
+        // Set timemodified.
+        $timemodified = time();
+
+        // Get matching records.
+        $records = $DB->get_recordset(
+            'local_coursetranslator',
+            array('t_id' => $objectid, 't_table' => $objecttable),
+            '',
+            'id'
+        );
+
+        // Update s_lastmodified time.
+        foreach ($records as $record) {
+            $DB->update_record(
+                'local_coursetranslator',
+                array('id' => $record->id, 's_lastmodified' => $timemodified )
+            );
+        }
+        $records->close();
+    }
+
+    /**
+     * Observer for course_section_updated event
+     *
+     * @param \core\event\course_section_updated $event
+     * @return void
+     */
+    public static function course_section_updated(\core\event\course_section_updated $event) {
+        global $DB;
+
+        // Get params.
+        $objectid = $event->objectid;
+        $objecttable = $event->objecttable;
+
+        // Set timemodified.
+        $timemodified = time();
+
+        // Get matching records.
+        $records = $DB->get_recordset(
+            'local_coursetranslator',
+            array('t_id' => $objectid, 't_table' => $objecttable),
+            '',
+            'id'
+        );
+
+        // Update s_lastmodified time.
+        foreach ($records as $record) {
+            $DB->update_record(
+                'local_coursetranslator',
+                array('id' => $record->id, 's_lastmodified' => $timemodified )
+            );
+        }
+        $records->close();
+    }
+
+    /**
+     * Observer for course_module_updated event
+     *
+     * @param \core\event\course_module_updated $event
+     * @return void
+     */
+    public static function course_module_updated(\core\event\course_module_updated $event) {
+        global $DB;
+
+        // Get params.
+        $objectid = $event->other['instanceid'];
+        $objecttable = $event->other['modulename'];
+
+        // Set timemodified.
+        $timemodified = time();
+
+        // Get matching records.
+        $records = $DB->get_recordset(
+            'local_coursetranslator',
+            array('t_id' => $objectid, 't_table' => $objecttable),
+            '',
+            '*'
+        );
+
+        // Update s_lastmodified time.
+        foreach ($records as $record) {
+            $DB->update_record(
+                'local_coursetranslator',
+                array('id' => $record->id, 's_lastmodified' => $timemodified )
+            );
+        }
+        $records->close();
+    }
+
+}
---
+++ db/events.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Course Translator Observers
+ *
+ * Watch for course, course section, and mod updates
+ *
+ * @package    local_coursetranslator
+ * @copyright  2022 Kaleb Heitzman <kaleb@jamfire.io>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @see        https://docs.moodle.org/dev/Events_API
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Event observer for local_coursetranslator.
+$observers = array(
+
+    array(
+        'eventname' => '\core\event\course_updated',
+        'callback'  => 'local_coursetranslator_observer::course_updated'
+    ),
+
+    array(
+        'eventname' => '\core\event\course_section_updated',
+        'callback'  => 'local_coursetranslator_observer::course_section_updated'
+    ),
+
+    array(
+        'eventname' => '\core\event\course_module_updated',
+        'callback'  => 'local_coursetranslator_observer::course_module_updated'
+    ),
+
+);

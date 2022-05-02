92dd84ba3383962e1a5797afbe966fb038fb0cae
Kaleb Heitzman <kaleb@jamfire.io>
2022-05-02 00:29:26 -0400

    0.9.3 (#8)
    * CI (#6)
    
    * fix supported version
    
    * Create dependency-review.yml (#4)
    
    * Create codeql.yml (#5)
    
    * added moodle ci plugin
    
    * updated phpdocs for observer
    
    * updated template context example
    
    * updated template context example
    
    * updated template context example
    
    * removed phpunit test check
    
    * added MOODLE_400_STABLE to action matrix
    
    * removed MOODLE_400_STABLE from workflow
    
    * added action badges
    
    * removed codeql
    
    * Create sonarcloud.yml (#7)
    
    * updated sonarcloud action
    
    * fixed duplicate status string
    
    * updated readme
    
    * updated sonarcloud name
    
    * updated readme
    
    * updated changelog
    
    * updated sonarcloud integration
    
    * updated sonarcloud integration
    
    * Tests (#9)
    
    * enable unittests
    
    * added release script
    
    * updated version

--- tests/settings_test.php
+++ tests/settings_test.php
@@ -15,22 +15,25 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Local Course Translator
+ * Local Course Translator Web Service
+ *
+ * Adds a webservice available via ajax for the Translate Content page.
  *
  * @package    local_coursetranslator
  * @copyright  2022 Kaleb Heitzman <kaleb@jamfire.io>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @see        https://docs.moodle.org/dev/version.php
+ * @see        https://docs.moodle.org/dev/PHPUnit
  */
 
-defined('MOODLE_INTERNAL') || die();
+namespace local_coursetranslator;
 
-$plugin->component    = 'local_coursetranslator';  // Full name of the plugin (used for diagnostics).
-$plugin->version      = 2022050200;                // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires     = 2020061500;                // Requires Moodle 3.9 LTS.
-$plugin->supported    = [39, 400];                // Supported Moodle Versions.
-$plugin->maturity     = MATURITY_ALPHA;            // Maturity level.
-$plugin->release      = 'v0.9.3';                  // Semantic Versioning for CHANGES.md.
-$plugin->dependencies = array(                     // Dependencies.
-    'filter_multilang2' => 2020101300
-);
+/**
+ * Settings Test
+ */
+class settings_test extends \advanced_testcase {
+
+    public function test_usedeepl() {
+        $this->assertEquals(2, 1 + 1);
+    }
+
+}

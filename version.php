78e66fe4da3b32566b81a9c2c8fbac757ff7f4b5
Bruno Baudry <bruno.baudry@bfh.ch>
2024-03-28 16:35:34 +0100

    Version number.

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_coursetranslator';  // Full name of the plugin (used for diagnostics).
-$plugin->version = 2024031900;                // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2024032800;                // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2020061500;                // Requires Moodle 3.9 LTS.
 $plugin->supported = [401, 403];                // Supported Moodle Versions.
 $plugin->maturity = MATURITY_ALPHA;            // Maturity level.
-$plugin->release = 'v0.9.7';                  // Semantic Versioning for CHANGES.md.
+$plugin->release = 'v0.9.8';                  // Semantic Versioning for CHANGES.md.
 // Dependencies.
 $plugin->dependencies = ['filter_multilang2' => 2020101300];
